6c57f1a3c2d11c149506c7ed425464ed0b477a8d
Ingvar Derešivski <inde999@gmail.com>
2023-11-07 15:58:23 +0200

    07.11.2023

---
+++ 07.11.2023/HelloWorldString/HelloWorldString/HelloWorldString/Program.cs
@@ -0,0 +1,15 @@
+﻿//namespace on selleks, et näha projekti nime
+//tegemist on projekti aadressiga
+namespace HelloWorldString
+{
+    //classi nimi
+    //internal tähendab classi ligipääsu
+    internal class Program
+    {
+        //meetod Main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 07.11.2023/Intro/IntroToC#/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter your favorite football player t-shirt number!");
+
+string number = Console.ReadLine();
+
+Console.WriteLine("Sinu number oli:");
+Console.WriteLine(number);
