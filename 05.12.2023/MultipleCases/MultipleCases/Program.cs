2036bbe81b92c760c87b04371b3a974f18fabd70
Ingvar Derešivski <inde999@gmail.com>
2023-12-05 15:50:52 +0200

    05.12.2023

---
+++ 05.12.2023/MultipleCases/MultipleCases/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace MultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiple Cases");
+
+            string topic, category;
+
+            topic = "Inheritance";
+
+            switch (topic)
+            {
+                case "Intro to C#":
+                case "Variables":
+                case "Data Types":
+
+                    category = "Basic";
+                    break;
+
+
+                case "Loops":
+                case "If statements":
+                case "Jump Statements":
+
+                    category = "Control Flow";
+                    break;
+
+                case "Class & Objects":
+                case "Inheritance":
+                case "Constructors":
+
+                    category = "OOP-s Concept";
+                    break;
+
+                default:
+                    category = "UNKNOWN";
+                    break;
+            }
+            Console.WriteLine("Category is " + category);
+        }
+    }
+}
---
+++ 05.12.2023/SwitchMethodCalling/SwitchMethodCalling/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SwitchMethodCalling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Teha swicth rakendus, kus on mitu
+            //case-i meetodi välja kutsumiseks
+
+            string carTypes = Console.ReadLine();
+
+            switch (carTypes)
+            {
+                case "coupe":
+                    CarCoupe(); //meetodi v'lja kutsumine
+                    break;
+
+                case "truck":
+                    Truck();//meetodi v'lja kutsumine
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+
+        static void CarCoupe() //uus meetod nimega CarCoupe()
+        {
+            Console.WriteLine("You have a coupe car"); //meetodi sisu
+        }
+
+        static void Truck()
+        {
+            Console.WriteLine("You have a truck");
+        }
+    }
+}
