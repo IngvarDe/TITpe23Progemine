a6b3e2b7375c19e0ecfbd03c9d8d33318ccd2a5f
Ingvar Derešivski <inde999@gmail.com>
2023-12-07 15:53:18 +0200

    07.12.2023

---
+++ 07.12.2023/Switch/Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            //peab saama sisestada numbrit ja muudab selle int andmetüübiks
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //tuleb teha switch, kus on esimene case 10, teine on 20 ja kolmas on 30
+            //kindlasti tuleb ka default lõppu teha
+            switch (num)
+            {
+                case 10:
+                    Console.WriteLine("Sisestasid nr 10");
+                    break;
+
+                case 20:
+                    Console.WriteLine("Sisestasid nr 20");
+                    break;
+
+                case 30:
+                    Console.WriteLine("Sisestasid nr 30");
+                    break;
+
+                default: 
+                    Console.WriteLine("Sisestasid mingi muu nr");
+                    break;
+            }
+        }
+    }
+}
---
+++ 07.12.2023/Switch2/Switch2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Switch2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number string andmetüübina");
+
+            string number = Console.ReadLine();
+
+            //case 1 ja case 2 puhul on üks ja sama väljund
+            //kus annab vastuseks: Valisid 1 või 2
+            //Ülejäänud juhtumid annab vastuseks:
+            //Valisid midagi muud
+            switch (number)
+            {
+                case "1":
+                case "2":
+                    Console.WriteLine("Valisid 1 või 2");
+                    break;
+
+                default: 
+                    Console.WriteLine("Valisid midagi muud");
+                    break;
+            }
+
+        }
+    }
+}
