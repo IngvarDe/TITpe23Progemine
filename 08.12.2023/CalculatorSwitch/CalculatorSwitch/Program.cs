0cdab39325682f4cdd67e58fe3f3732da453a162
Ingvar Derešivski <inde999@gmail.com>
2023-12-08 15:50:43 +0200

    08.12.2023

---
+++ 08.12.2023/CalculatorSwitch/CalculatorSwitch/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace CalculatorSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkulaator");
+
+            //vaja sisestada nr konsooli ja 
+            //muudab selle int andmetüübiks
+            Console.WriteLine("Sisesta esimene nr");
+            float firstNr = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisesta tehe");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Sisesta teine nr");
+            float secondNr = float.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    Add(firstNr, secondNr);
+                    break;
+
+                case "-":
+                    Subtract(firstNr, secondNr);
+                    break;
+
+                case "*":
+                    Multiple(firstNr, secondNr);
+                    break;
+
+                case "/":
+                    Divide(firstNr, secondNr);
+                    break;
+            }
+
+        }
+
+        static void Add(float firstNr, float secondNr)
+        {
+            float resultAdd = firstNr + secondNr;
+            Console.WriteLine("Tulemus: " + resultAdd);
+        }
+
+        static void Subtract(float firstNr, float secondNr)
+        {
+            float resultAdd = firstNr - secondNr;
+            Console.WriteLine("Tulemus: " + resultAdd);
+        }
+
+        static void Multiple(float firstNr, float secondNr)
+        {
+            float resultAdd = firstNr * secondNr;
+            Console.WriteLine("Tulemus: " + resultAdd);
+        }
+
+        static void Divide(float firstNr, float secondNr)
+        {
+            float resultAdd = firstNr / secondNr;
+            Console.WriteLine("Tulemus: " + resultAdd);
+        }
+    }
+}
---
+++ 08.12.2023/Switch2/Switch2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Switch2
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Võrdlemine");
+
+            //esimese koondtulemuse puhul on number vahemikus
+            //0 kuni 100,
+            //Teise puhul 101 kuni 200 ja
+            //kolmanda puhul 201 kuni 300
+            string number = Console.ReadLine();
+            int nr = int.Parse(number);
+
+            switch (nr)
+            {
+                case > 0 and <= 100:
+                    Console.WriteLine("Valisid 0-st kuni 100-ni");
+                    break;
+
+                case > 101 and <= 200:
+                    Console.WriteLine("Valisid 101-st kuni 200-ni");
+                    break;
+
+                case > 201 and <= 300:
+                    Console.WriteLine("Valisid 201-st kuni 300-ni");
+                    break;
+            }
+
+        }
+    }
+}
