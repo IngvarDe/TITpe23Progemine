d2cf9aa8cb9e45d4203f30b51f9fddc58a368c92
Ingvar Derešivski <inde999@gmail.com>
2023-12-14 15:57:47 +0200

    14.12.2023

---
+++ 14.12.2023/KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kujundi kalkulaator");
+            //Ruudu sees asub ring. Ringi raadius on 3.
+            //Leia ja väljasta ekraanile ruudu pindala ja ümbermõõt.
+
+            int roundRadius = 3;
+            //int roundDiameter = 6;
+
+            Console.WriteLine("Ruudu pindala: " + (2 * roundRadius) * (2 * roundRadius));
+            Console.WriteLine("Ruudu ümbermõõt: " + (2 * roundRadius) * 4);
+
+            //Leia ja väljasta ekraanile ringi pindala ja ümbermõõt.
+            Console.WriteLine("Ringi  pindala: " + Math.PI * (roundRadius * roundRadius));
+            Console.WriteLine("Ringi ümbermõõt: " + 2 * Math.PI * roundRadius);
+        }
+    }
+}
---
+++ 14.12.2023/SulgudeKasutamine/SulgudeKasutamine/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SulgudeKasutamine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sulgude kasutamine!");
+            Console.WriteLine("Kui palju on: 3 + 8 / (4 - 2) * 4 = ?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            double calculation2 = 3 + 8 / 4 - 2 * 4;
+            double calculation3 = (3 + 8) / (4 - 2) * 4;
+            double calculation4 = 3 + 8 / ((4 - 2) * 4);
+
+            Console.WriteLine("Vastus: " + calculation1);
+            Console.WriteLine("Vastus: " + calculation2);
+            Console.WriteLine("Vastus: " + calculation3);
+            Console.WriteLine("Vastus: " + calculation4);
+        }
+    }
+}
