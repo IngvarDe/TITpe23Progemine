08c1ff17cdffb9dc1cf89be38be1d0299560ed32
Ingvar Derešivski <inde999@gmail.com>
2023-12-18 14:07:28 +0200

    18.12.2023

---
+++ 18.12.2023/ForEndless/ForEndless/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ForEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lõputu for loop");
+
+            //1. hakkab itereerima lõputult
+            //kasutada for loopi
+
+            //2. aga kui nr jõuab 10 peale, siis lõpetab ära
+            //VIHJE: kasutage break-i
+            ushort i = 0;
+
+            for (; ; )
+            {
+                if (i < 10)
+                {
+                    Console.WriteLine("i v''rtus on: {0}", i);
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
