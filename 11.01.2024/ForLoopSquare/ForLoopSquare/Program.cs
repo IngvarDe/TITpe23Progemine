3767c4e4f4537701ebf175173315d5d1f256a827
Ingvar Derešivski <inde999@gmail.com>
2024-01-11 15:53:39 +0200

    11.01.2024

---
+++ 11.01.2024/ForLoopSquare/ForLoopSquare/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ForLoopSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ruut");
+            Console.WriteLine("Sisesta ruudu suurus");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ruudu pindala ja ümbermõõt:");
+            //Ruudu pindala on ... ja ümbermmõõt on ...
+            Console.WriteLine("Ruudu pindala on " + size * size + 
+                " ja ümbermmõõt on " + size * 4 + " ühikut");
+
+            for (int row = 1; row <= size; row++)
+            {
+                for (int column = 1; column <= size * 2; column++)
+                {
+                    string mark;
+                    if (row == column || row + column <= size + 1)
+                    {
+                        mark = "*";
+                    }
+                    else
+                    {
+                        mark = "*";
+                    }
+                    Console.Write(mark);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 11.01.2024/NumberPyramid/NumberPyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            int t = 0;
+            Console.WriteLine("Numbri püramiid");
+
+            Console.WriteLine("Sisesta ridade arv");
+
+            rows = Convert.ToInt32(Console.ReadLine());
+
+
+            for (i = 0; i <= rows; i++)
+            {
+                for (j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("{0} ", t++);
+                    //Console.Write("*"); //kui paned selle, siis saad püramiidi
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
