f25335c573edb09e904addf0d14e9163e8534de8
Ingvar Derešivski <inde999@gmail.com>
2024-01-30 15:47:06 +0200

    30.01.2024

---
+++ 20. 30.01.2024/ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = { "H", "e", "l", "l", "o" };
+
+            //vaja for loopiga iga täht konsooli saada
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
