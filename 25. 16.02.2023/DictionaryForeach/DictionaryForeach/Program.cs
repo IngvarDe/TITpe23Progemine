2a458c2bdcbcca663cd8b3cdd2313cd31a07cbe6
Ingvar Derešivski <inde999@gmail.com>
2024-02-16 13:33:24 +0200

    16.02

---
+++ 25. 16.02.2023/DictionaryForeach/DictionaryForeach/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DictionaryForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionary ja foreach");
+
+            var domains = new Dictionary<string, string>()
+            {
+                {"est", "Estonia" },
+                {"lat", "Latvia" },
+                {"fin", "Finland" },
+                {"swe", "Sweden" }
+            };
+            //kasutate foreachi ja juurde tuleb kolmas rida
+            int i = 1;
+            foreach (var pair in domains)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} - {i}");
+                i++;
+            }
+            //see kood githubi panna
+        }
+    }
+}
---
+++ 25. 16.02.2023/ForeachInForeach/ForeachInForeach/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ForeachInForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foreach nesting");
+
+            var words = new List<List<string>>
+            {
+                new List<string> {"tea", "coffee", "juice", "smoothie"},
+                new List<string> {"cup", "plate", "knife", "spoon"},
+                new List<string> {"rain", "snow", "hailstorm", "sunny"}
+            };
+            //foreach on foreachi sees
+            foreach (var word in words)
+            {
+                foreach (var item in word)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
