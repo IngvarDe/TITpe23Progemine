3ca8196118a2b51ffb17ea6bd164c34c353a6b4a
Ingvar Derešivski <inde999@gmail.com>
2024-03-08 14:10:11 +0200

    08.03.2024

---
+++ 29. 08.03.2024/FileReaderAndWriter/FileReaderAndWriter/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace FileReaderAndWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            /*
+            >>>>>>>>>>
+            Loo fail tuttavad.txt ja lisa sinna vähemalt 6 tuttava perekonna- 
+            ja eesnimed(iga tuttav uuele reale, perekonna- ja eesnimi 
+            tühikuga eraldatult). Koosta programm, mis loeb failist 
+            andmed ja väljastab need ekraanile tähestikulises järjekorras.
+            Mõistlik on ilmselt kasutada järjendit ja sellega
+            seonduvaid võimalusi(järjestamist). Tähestikulises 
+            järjekorras salvestage tuttavate nimed ka uude faili tuttavad1.txt.
+            */
+            ReaderAndWriter();
+        }
+
+        static void ReaderAndWriter()
+        {
+            try 
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/Ingvar/Desktop/tuttavad.txt"))
+                {
+                    string rida = sr.ReadToEnd();
+                    string[] nimed = rida.Split('\n');
+                    foreach (string name in nimed)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                    Array.Sort(nimed);
+                    //sorteerida nimed t'hestikulises j'rjestus
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("Sorteeritud....");
+                    foreach (string name in nimed)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    sr.Close();
+
+                    //kirjutada kogu see info failile, mille nimi on tuttavad1
+
+                    using (StreamWriter kirjuta = new StreamWriter("C:/Users/Ingvar/Desktop/tuttavad1.txt", true))
+                    {
+                        int i = 1;
+                        Console.WriteLine("Sorteeritud fail nimekirja");
+                        foreach (string name in nimed)
+                        {
+                            kirjuta.WriteLine(i + " " + name);
+                            i++;
+                        }
+                        //kirjuta.WriteLine("");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Mingi error");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
---
+++ 29. 08.03.2024/FileWordReader/FileWordReader/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace FileWordReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luuletus");
+
+            FileWordReading();
+        }
+
+        static void FileWordReading()
+        {
+            string line; //s]na muutuja deklareerimine
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/Ingvar/Desktop/luuletus.txt"))
+                {
+                    int cnt = 0; //rea muutuja
+                    while (sr.EndOfStream == false)
+                    {
+                        ++cnt; // rea nr suurendamine 1 [hiku v]rra
+                        line = sr.ReadLine(); //teksti lugemine txt failist
+                        int pikkus = line.Length; //s]na lugemine, kui s]ne pikkusena
+                        Console.WriteLine(cnt + " " + line + " " + pikkus); //kogu
+                    }
+                    sr.Close(); //faili lugemise l]petamine
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Faili ei saa lugeda");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
