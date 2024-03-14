368d01dc4ae931772650801e9989886df6a92462
Ingvar Derešivski <inde999@gmail.com>
2024-03-14 11:44:23 +0200

    08.03.2024

---
+++ 29. 08.03.2024/EuroSendid/EuroSendid/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EuroSendid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ülesanne 6
+            >>>>>>>>>>
+            Eurosendid on väärtuses 1, 2, 5, 10, 20 ja 50. Koostage funktsioon,
+            millele antakse argumendina rahasumma (sentides) ja mis trükib välja, 
+            kui palju milliseid münte peaks selles vääringus andma, et müntide 
+            arv oleks väikseim. Näiteks 37 sendi puhul 1 20-sendine, 1 10-sendine, 
+            1 5-sendine ja 1 2-sendine. Koostage järjend, mille elementideks
+            on teisendatavad rahasummad. Koostage programm, mis võtab järjendist 
+            elemente ja kutsub igaühe korral neist välja ülaltoodud funktsiooni. 
+            Probleeme tekitab kindlasti korrektse lause koostamine (mida teha, 
+            kui mingis vääringus münte ei vajata? mida teha ainsuse ja mitmusega?).
+            Püüdke leida parim võimalik lahendus.
+            */
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
