9e5ffb4e6441a8b0142f12ed4ff5529cf47e0972
Ingvar Derešivski <inde999@gmail.com>
2024-03-15 14:16:20 +0200

    15.03.2024

---
+++ 31. 15.03.2024/LINQ/LINQ/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kutsume esile LINQ läbi switchi");
+            Console.WriteLine("Vali vastav link numbriga");
+            Console.WriteLine("1. Where");
+
+            int choice = int.Parse(Console.ReadLine());
+
+
+            switch (choice)
+            {
+                case 1:
+                    WhereLINQ();
+
+                    break;
+
+                default:
+                    Console.WriteLine("Valikut ei tehtud");
+                    break;
+            }
+        }
+
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleList.peoples
+                .Where(x => x.Age > 20 && x.Age < 23);
+
+            foreach (var people in peopleAge)
+            {
+                Console.WriteLine(people.Name);
+            }
+        }
+
+        public static void WhereByNameLINQ()
+        {
+            Console.WriteLine("Kirjuta inimese nimi:");
+            string name = Console.ReadLine();
+
+            var peopleAge = PeopleList.peoples
+                .Where(x => x.Name == name);
+
+            foreach (var people in peopleAge)
+            {
+                Console.WriteLine(people.Name);
+            }
+        }
+
+        //nime p]hjal teha filtreerimine ja kasutada where
+        //kutsuda see meetod esile Main meetodi switchi sees
+
+        public static void ThenByLINQ()
+        {
+
+        }
+
+    }
+}
