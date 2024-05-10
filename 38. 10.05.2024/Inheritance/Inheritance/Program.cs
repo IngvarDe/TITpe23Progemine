a6fa73da7edd90fa99abb6fc986d8e7d12d5ff9a
Ingvar Derešivski <inde999@gmail.com>
2024-05-10 12:17:41 +0300

    Inheritance

---
+++ 38. 10.05.2024/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert two numbers: ");
+            Console.WriteLine("First number: ");
+            int firstNr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second number: ");
+            int secondNr = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetWidth(firstNr);
+            rectangle.SetHeight(secondNr);
+
+            Console.WriteLine("Total area: {0}", rectangle.GetArea());
+        }
+    }
+
+    class Shape
+    {
+        public void SetWidth(int w)
+        {
+            width = w;
+        }
+
+        public void SetHeight(int h)
+        {
+            height = h;
+        }
+
+        protected int width;
+        protected int height;
+    }
+
+    class Rectangle : Shape
+    {
+        public int GetArea()
+        {
+            return (width * height);
+        }
+    }
+}
